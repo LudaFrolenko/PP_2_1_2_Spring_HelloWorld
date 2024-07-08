cebe68e58eefd449add0a8c4d47359c43f980735
LIUDMYLA FROLENKO <ddreddln@gmail.com>
2024-07-08 14:19:45 +0400

    cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,23 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+
+
+
         System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        System.out.println(bean == bean1);
+
+        System.out.println(bean2.getMessage());
+        System.out.println(bean3.getMessage());
+        System.out.println(bean2 == bean3);
+
+
     }
 }
